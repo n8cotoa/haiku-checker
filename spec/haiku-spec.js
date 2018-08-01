57742e6cad113f041bdda0c9dc1ae8757d77d64a
Nate Cottle and Thad Donaghue <nate+thad@>
2018-08-01 16:43:10 -0700

    fix scoping issue, add API call

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -9,4 +9,31 @@ describe('Haiku', function(){
   it('will construct the haiku', function(){
     expect(haiku.line1).toEqual('I am first with five');
   });
+
+  it('will count the syllables in each line and return those numbers', function (){
+    let count = haiku.countSyllables();
+    expect(count).toEqual([5, 7, 5]);
+  });
+
+  it('will count for fringe case "idea", "acme", "aria"', function(){
+    let testHaiku = new Haiku('idea', 'acme', 'aria');
+    let count = testHaiku.countSyllables();
+    expect(count).toEqual([3, 2, 3]);
+  });
+
+  it('will count for fringe case "boise"', function(){
+    let testHaiku = new Haiku('boise', 'acme', 'aria');
+    let count = testHaiku.countSyllables();
+    expect(count[0]).toEqual(2);
+  });
+
+  it('will return true if inputed poem is a haiku', function(){
+    expect(haiku.isHaiku()).toEqual(true)
+  });
+
+  it('will return false if inputed poem is not a haiku', function(){
+    let testHaiku = new Haiku('idea', 'acme', 'aria');
+    expect(testHaiku.isHaiku()).toEqual(false)
+  });
+
 });
--- src/haiku.js
+++ src/haiku.js
@@ -3,6 +3,29 @@ export class Haiku {
     this.line1 = line1;
     this.line2 = line2;
     this.line3 = line3;
+    this.syllables = 0;
+  }
+
+  getSyllables(input) {
+    var request = new XMLHttpRequest();
+    request.open("GET", `https://api.datamuse.com/words?sl=${input}&max=1&md=s`);
+    var self = this;
+
+    request.onload = function() {
+      if(this.status == 200) {
+        let word = JSON.parse(this.response);
+        self.syllables = word[0].numSyllables;
+        console.log(request.responseText);
+
+        //return num;
+      }
+    }
+
+    request.onerror = function() {
+      console.log("Unable to connect to the server API.");
+    }
+
+    request.send();
   }
 
 
@@ -12,7 +35,18 @@ export class Haiku {
     for(let i = 0; i < lines.length; i++) {
       let words = lines[i].toLowerCase().split(" ");
       for(let j = 0; j < words.length; j++) {
-        if (words[j].charAt(words[j].length - 1) == "e" && words[j].match(/[aiouy]/)) {
+        if(words[j].charAt(0).match(/[aiouy]/) && words[j].charAt(words[j].length -1) == "a") {
+          let newString = words[j].substring(0, (words[j].length -1))
+          newString += " a";
+          let syllables = newString.match(/[aiouy]+e*|e(?!d$|ly).|[td]ed|le$/gm);
+          count[i] += syllables.length;
+        }
+        else if(words[j].charAt(0) == "a" && words[j].charAt(words[j].length -1) == "e") {
+          words[j] += " ";
+          let syllables = words[j].match(/[aiouy]+e*|e(?!d$|ly).|[td]ed|le$/gm);
+          count[i] += syllables.length;
+        }
+        else if (words[j].charAt(words[j].length - 1) == "e" && words[j].match(/[aiouy]/)) {
           let syllables = words[j].match(/[aiouy]+e*|e(?!d$|ly).|[td]ed|le$/gm);
           count[i] += syllables.length;
         } else {
